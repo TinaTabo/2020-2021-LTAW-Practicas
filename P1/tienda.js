542da10582e2b778a21c3b2b045b07a6cbf1a17c
TinaTabo <tinatabo98@gmail.com>
2021-02-23 13:39:58 +0100

    Comenzando el servidor( back-end)

---
+++ P1/tienda.js
@@ -0,0 +1,23 @@
+//-- Servidor de mi tienda de música.
+const http = require('http');
+const fs = require('fs');
+
+//-- Puerto a utilizar
+const PUERTO = 9000
+
+//-- Crear el servidor. Por cada petición recibida
+//-- se imprime un mensaje de control en el terminal
+const server = http.createServer((req, res) => {
+    console.log("Petición recibida");
+
+    //-- Aqui se va a programar la lectura de 
+    //-- ficheros que el servidor devuelva las 
+    //-- paginas correspondientes.
+
+})
+
+//-- Activar el servidor
+server.listen(PUERTO);
+
+console.log("Servidor Activado!. Escuchando en el puerto " + PUERTO);
+
