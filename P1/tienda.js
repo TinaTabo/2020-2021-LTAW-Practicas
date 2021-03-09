dab13bd970b030c7a33c62af95715589f377a1ac
TinaTabo <tinatabo98@gmail.com>
2021-03-09 13:30:28 +0100

    obteniendo petición en el servidor

--- P1/tienda.js
+++ P1/tienda.js
@@ -3,20 +3,38 @@ const http = require('http');
 const fs = require('fs');
 const url = require('url');
 
-//-- Puerto a utilizar
+//-- Puerto
 const PUERTO = 9000
 
 //-- Crear el servidor. Por cada petición recibida
 //-- se imprime un mensaje de control en el terminal
 const server = http.createServer((req, res) => {
-    console.log("Petición recibida");
-
-    //-- Aqui se va a programar la lectura de 
-    //-- ficheros que el servidor devuelva las 
-    //-- paginas correspondientes a las distintas 
-    //-- peticiones.
+    console.log("----Petición recibida----");
     
+    //-- Obtener y parsear la url de la petición
+    let myURL = url.parse(req.url, true);
+    console.log("Recurso solicitado (URL): " + req.url);
+    console.log("Recurso: " + myURL.pathname);
 
+    //-- Dependiendo del pathname, se define el fichero
+    //-- con el que se responderá la petición
+
+    //-- inicializamos el fichero vacío
+    let fich = "";
+
+    //-- Obtenemos el fichero correspondiente.
+    if(myURL.pathname == '/'){
+        fich += "tienda.html"; //-- Página principal de la tienda
+    }else{
+        fich += myURL.pathname; //-- Otro recurso.
+    }
+
+    //-- Obtenemos el tipo de fichero para saber qué devolver al cliente.
+    fich_type = fich.split(".")[1]; //-- Extension del archivo.
+    fich = "." + fich; //-- Leer el archivo.
+    //-- Comprobamos los parámetros obtenidos por el terminal
+    console.log("Nombre del Fichero: " + fich);
+    console.log("Tipo de Fichero: " + fich_type);
 })
 
 //-- Activar el servidor
