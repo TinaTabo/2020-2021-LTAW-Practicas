0c450e9cde6bb8bc292d42e803c08ba77cc69b44
TinaTabo <tinatabo98@gmail.com>
2021-04-06 12:39:19 +0200

    ejercicio 2 l5

--- P2/L5/tienda.js
+++ P2/L5/tienda.js
@@ -27,3 +27,31 @@ tienda[1]["usuarios"].forEach((element, index)=>{
     console.log("Usuario " + (index + 1) + ": " + element.user);
   });
 console.log();
+
+//-- Imprimir numero de productos de la tienda.
+console.log("Hay " + tienda[0]["productos"].length + " productos en la tienda.");
+
+//-- Imprimir lista de productos.
+console.log("Lista de productos");
+console.log("-----------------------------");
+tienda[0]["productos"].forEach((element, index)=>{
+    console.log("Producto " + (index + 1) + ": " + element.nombre);
+  });
+console.log();
+
+//-- Imprimir numero de pedidos
+console.log("Hay " + tienda[2]["pedidos"].length + " pedidos en la tienda.");
+
+//-- Imprimir lista de pedidos.
+console.log("Lista de pedidos");
+console.log("-----------------------------");
+tienda[2]["pedidos"].forEach((element, index)=>{
+    console.log("Detalles del Pedido " + (index + 1) + "\n" +
+                "Usuario: " + element.user + "\n" +
+                "Dirección de envío: " + element.dirección + "\n" +
+                "Número de la tarjeta: " + element.tarjeta);
+                element.productos.forEach((element, index) => {
+                    console.log("Producto " + (index + 1) + ": " + element.producto)
+                });
+  });
+console.log();
