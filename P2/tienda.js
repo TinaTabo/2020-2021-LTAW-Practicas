992e0a434f0a5bf2b05c4017823ede6c5b340626
TinaTabo <tinatabo98@gmail.com>
2021-04-29 19:44:21 +0200

    añadido tramite de pedido

--- P2/tienda.js
+++ P2/tienda.js
@@ -8,14 +8,17 @@ const PUERTO = 9000;
 const INICIO = fs.readFileSync('inicio.html', 'utf-8');
 
 //-- Cargar pagina web del formulario login
-const FORMULARIO = fs.readFileSync('login.html','utf-8');
+const FORMULARIO_LOGIN = fs.readFileSync('login.html','utf-8');
+const FORMULARIO_PEDIDO = fs.readFileSync('pedido.html','utf-8');
 
 //-- Cargar las paginas de respuesta
 const LOGIN_OK = fs.readFileSync('login-ok.html','utf-8');
 const LOGIN_KO = fs.readFileSync('login-ko.html','utf-8');
+const PEDIDO_OK = fs.readFileSync('pedido-ok.html', 'utf-8');
 
 //-- Registro -> Fichero JSON
 const FICHERO_JSON = "tienda.json";
+const FICHERO_JSON_PRUEBA = "tienda_prueba.json";
 
 //-- Leer el fichero JSON (lectura sincrona)
 const  tienda_json = fs.readFileSync(FICHERO_JSON);
@@ -44,10 +47,6 @@ const server = http.createServer((req, res) => {
     console.log("  Ruta: " + myURL.pathname);
     console.log("  Parametros: " + myURL.searchParams);
 
-    //-- Obtener el nombre de usuario
-    let user = myURL.searchParams.get('nombre');
-    console.log('Nombre: ' + user);
-
     //-- Por defecto -> pagina de inicio
     let content_type = "text/html";
     let content = INICIO;
@@ -55,13 +54,16 @@ const server = http.createServer((req, res) => {
     //-- Acceso al formulario login
     if (myURL.pathname == '/login') {
         content_type = "text/html";
-        content = FORMULARIO;
+        content = FORMULARIO_LOGIN;
     }
 
-    //-- Procesar la respuesta del formulario
-    if (myURL.pathname == '/procesar') {
-        content_type = "text/html";
+    //-- Procesar la respuesta del formulario login
+    if (myURL.pathname == '/procesarlogin') {
+        //-- Obtener el nombre de usuario
+        let user = myURL.searchParams.get('nombre');
+        console.log('Nombre: ' + user);
         //-- Dar bienvenida solo a usuarios registrados.
+        content_type = "text/html";
         if (users_reg.includes(user)){
             console.log('El usuario esta registrado');
             content = LOGIN_OK;
@@ -71,6 +73,47 @@ const server = http.createServer((req, res) => {
             content = LOGIN_KO;
         }
     }
+
+    //-- Acceso al formulario pedido
+    if (myURL.pathname == '/pedido') {
+      content_type = "text/html";
+      content = FORMULARIO_PEDIDO;
+    }
+
+    //-- Procesar la respuesta del formulario pedido
+    if (myURL.pathname == '/procesarpedido') {
+      //-- Guardar los datos del pedido en el fichero JSON
+      //-- Primero obtenemos los parametros
+      let direccion = myURL.searchParams.get('dirección');
+      let tarjeta = myURL.searchParams.get('tarjeta');
+      console.log("Dirección de envío: " + direccion + "\n" +
+                  "Número de la tarjeta: " + tarjeta + "\n");
+      //-- Guardar datos del pedido en el registro tienda.json
+      //-- si este no es nulo (null)
+      if ((direccion != null) && (tarjeta != null)) {
+        let pedido = {
+          "user": "root",
+          "dirección": direccion,
+          "tarjeta": tarjeta,
+          "productos": [
+            {
+              "producto": "guitarra"
+            },
+            {
+              "producto": "piano"
+            }
+          ]
+        }
+        tienda[2]["pedidos"].push(pedido);
+        //-- Convertir a JSON y registrarlo
+        let myTienda = JSON.stringify(tienda, null, 4);
+        fs.writeFileSync(FICHERO_JSON_PRUEBA, myTienda);
+      }
+      //-- Confirmar pedido
+      content_type = "text/html";
+      console.log('Pedido procesado correctamente');
+      content = PEDIDO_OK;
+     }
 
     //-- Si hay datos en el cuerpo, se imprimen
   req.on('data', (cuerpo) => {
