cc303db54ba02f72d9e8cff2165bb89c101350bc
tinatabo <tinatabo98@gmail.com>
2021-05-10 09:58:17 +0200

    contador de usuarios, dar bienvenida y despedida

--- P3/chat_server.js
+++ P3/chat_server.js
@@ -3,6 +3,7 @@ const socket = require('socket.io');
 const http = require('http');
 const express = require('express');
 const colors = require('colors');
+const { userInfo } = require('node:os');
 
 //-- Puerto donde se utilizará el chat.
 const PUERTO = 9000;
@@ -17,6 +18,7 @@ const command_list = "Estos son los comandos soportados por BangChat:<br>"
 const msg_hello = "Hello Army!";
 const msg_welcome = "Bienvenid@ a BangChat!";
 const msg_bye = "Bye Bye!";
+const msg_newuser = "Un/a nuev@ Army se ha unido al Chat";
 
 //-- Contador de usuarios conectados
 let users_count = 0;
@@ -36,7 +38,7 @@ const io = socket(server);
 //-------- PUNTOS DE ENTRADA DE LA APLICACION WEB
 //-- Definir el punto de entrada principal de mi aplicación web
 app.get('/', (req, res) => {
-  res.send('Bienvenido a mi aplicación Web!!!' + '<p><a href="/Ej-09.html">Test</a></p>');
+  res.send('Bienvenido a BangChat!!!' + '<p><a href="/chat_main.html">Ir a BangChat</a></p>');
 });
 
 //-- Esto es necesario para que el servidor le envíe al cliente la
@@ -44,17 +46,30 @@ app.get('/', (req, res) => {
 app.use('/', express.static(__dirname +'/'));
 
 //-- El directorio publico contiene ficheros estáticos
-app.use(express.static('public'));
+//--app.use(express.static('public')); <-- Lo tengo todo en la misma carpeta por lo que esto no hace falta.
 
 //------------------- GESTION SOCKETS IO
 //-- Evento: Nueva conexion recibida
 io.on('connect', (socket) => {
   
   console.log('** NUEVA CONEXIÓN **'.yellow);
+  //-- Contabilizar al nuevo usuario
+  users_count += 1;
+  
+  //-- Enviar mensaje de bienvenida al usuario.
+  socket.send(msg_welcome);
+
+  //-- Notificar al resto de usuarios que un nuevo
+  //-- usuario a accedido al chat.
+  socket.broadcast.emit('message', msg_newuser);
 
   //-- Evento de desconexión
   socket.on('disconnect', function(){
     console.log('** CONEXIÓN TERMINADA **'.yellow);
+    //-- Enviar mensaje de despedida al usuario.
+    socket.broadcast.emit('message', msg_bye);
+    //-- Actualizar el numero de usuarios conectados
+    users_count -= 1;
   });  
 
   //-- Mensaje recibido: Reenviarlo a todos los clientes conectados
