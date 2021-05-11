4c68ad0665547c86c508a5cdcf02e6844b05594e
tinatabo <tinatabo98@gmail.com>
2021-05-11 12:20:32 +0200

    version 10

--- P4/S10/index.js
+++ P4/S10/index.js
@@ -6,6 +6,7 @@ const display = document.getElementById("display");
 const info1 = document.getElementById("info1");
 const info2 = document.getElementById("info2");
 const info3 = document.getElementById("info3");
+const print = document.getElementById("print");
 
 //-- Acceder a la API de node para obtener la info
 //-- Sólo es posible si nos han dado permisos desde
@@ -13,7 +14,14 @@ const info3 = document.getElementById("info3");
 info1.textContent = process.arch;
 info2.textContent = process.platform;
 info3.textContent = process.cwd();
+
 btn_test.onclick = () => {
     display.innerHTML += "TEST! ";
     console.log("Botón apretado!");
 }
+
+//-- Mensaje recibido del proceso MAIN
+electron.ipcRenderer.on('print', (event, message) => {
+    console.log("Recibido: " + message);
+    print.textContent = message;
+});
